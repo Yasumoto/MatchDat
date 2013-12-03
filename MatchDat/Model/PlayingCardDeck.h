993ae8cced6d0e780d085fe67336026b1afc8a4d
Joe Smith <jsmith@twitter.com>
2013-12-02 23:04:59 -0800

    Playing Card Deck

---
+++ MatchDat/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  MatchDat
+//
+//  Created by Joseph Smith on 12/2/13.
+//  Copyright (c) 2013 bjoli. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
