9339ccd213e7ebd3410210b30385f3adaec8cb09
Joe Smith <jsmith@twitter.com>
2014-01-10 14:43:20 -0800

    SetCard for playing Set

---
+++ MatchDat/Model/SetCard.h
@@ -0,0 +1,30 @@
+//
+//  SetCard.h
+//  MatchDat
+//
+//  Created by Joseph Smith on 1/10/14.
+//  Copyright (c) 2014 bjoli. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+extern NSString *SET_SYMBOL_DIAMOND;// = @"▲";
+extern NSString *SET_SYMBOL_SQUIGGLE;// = @"●";
+extern NSString *SET_SYMBOL_OVAL;// = @"■";
+
+extern NSString *SET_SHADING_SOLID;
+extern NSString *SET_SHADING_STRIPED;
+extern NSString *SET_SHADING_OPEN;
+
+extern NSString *SET_COLOR_RED;
+extern NSString *SET_COLOR_GREEN;
+extern NSString *SET_COLOR_PURPLE;
+
+@property (nonatomic) NSUInteger number;
+@property (strong, nonatomic) NSString *symbol;
+@property (strong, nonatomic) NSString *shading;
+@property (strong, nonatomic) NSString *color;
+
+@end
---
+++ MatchDat/Model/SetCard.m
@@ -0,0 +1,85 @@
+//
+//  SetCard.m
+//  MatchDat
+//
+//  Created by Joseph Smith on 1/10/14.
+//  Copyright (c) 2014 bjoli. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@synthesize color = _color;
+@synthesize symbol = _symbol;
+@synthesize shading = _shading;
+
+NSString const *SET_SYMBOL_DIAMOND = @"▲";
+NSString const *SET_SYMBOL_SQUIGGLE = @"●";
+NSString const *SET_SYMBOL_OVAL = @"■";
+
+NSString const *SET_SHADING_SOLID = @"SOLID";
+NSString const *SET_SHADING_STRIPED = @"STRIPED";
+NSString const *SET_SHADING_OPEN = @"OPEN";
+
+NSString const *SET_COLOR_RED= @"RED";
+NSString const *SET_COLOR_GREEN = @"GREEN";
+NSString const *SET_COLOR_PURPLE = @"PURPLE";
+
+
+- (NSString *)contents {
+    return @"";
+}
+
++ (NSArray *) validSymbols {
+    return @[SET_SYMBOL_DIAMOND, SET_SYMBOL_OVAL, SET_SYMBOL_SQUIGGLE];
+}
+
++ (NSArray *) validShading {
+    return @[SET_SHADING_SOLID, SET_SHADING_STRIPED, SET_SHADING_OPEN];
+}
+
++ (NSArray *) validColors {
+    return @[SET_COLOR_GREEN, SET_COLOR_PURPLE, SET_COLOR_RED];
+}
+
+- (void) setSymbol:(NSString *)symbol {
+    if ([[SetCard validSymbols] containsObject:symbol]) {
+        _symbol = symbol;
+    }
+}
+
+- (void) setShading:(NSString *)shading {
+    if ([[SetCard validShading] containsObject:shading]) {
+        _shading = shading;
+    }
+}
+
+- (void) setColor:(NSString *)color {
+    if ([[SetCard validColors] containsObject:color]) {
+        _color = color;
+    }
+}
+
+- (NSString *) symbol {
+    return _symbol ? _symbol : @"?";
+}
+
+- (NSString *) color {
+    return _color ? _color : @"?";
+}
+
+- (NSString *) shading {
+    return _shading ? _shading : @"?";
+}
+
+- (int) match:(NSArray *)otherCards {
+    for (SetCard *otherCard in otherCards) {
+        if ([otherCard.symbol isEqualToString:self.symbol] || [otherCard.color isEqualToString:self.color] || [otherCard.shading isEqualToString:self.shading]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+@end
